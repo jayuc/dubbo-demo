b3c04ed008667fa9e77407c3797b8fa21fdda908
余杰 <402842327@qq.com>
2020-11-24 14:49:26 +0800

    修复问题

--- userServer/packDocker.sh
+++ userServer/packDocker.sh
@@ -14,11 +14,11 @@ else
   exit 5
 fi
 
-echo "step 1/3: pull code"
+echo "step 1/3: pull code ..."
 # step 1/3:
 # 获取代码
 if [ -e ./pom.xml ]; then
-  echo "code already exists, then git pull"
+  echo "code already exists, then git pull ..."
   git pull
 else
   git clone https://github.com/jayuc/dubbo-demo.git
@@ -26,13 +26,13 @@ fi
 
 # setp 2/3:
 # 编译项目
-echo "setp 2/3: mvn clean package"
+echo "setp 2/3: mvn clean package ..."
 mvn clean package -Dmaven.test.skip=true
 
 # setp 3/3:
 # 构建docker镜像
 # 注意：DockerFile 中的版本号要配置和项目版本号一致
-echo "setp 3/3: docker build"
+echo "setp 3/3: docker build ..."
 cp ./Dockerfile ./target/
 cd ./target/
 docker build -t "server/user:${m_version}" .
